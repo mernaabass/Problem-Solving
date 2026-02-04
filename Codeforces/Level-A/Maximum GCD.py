84a6c4982135643bd407057f308ef8ebfd2199c6
mernakabass <mernaabass@outlook.com>
2026-02-04 14:54:20 +0200

    Solved: Maximum GCD [Greedy, Implementation, Math, Number theory]

--- Codeforces/Level-A/Maximum GCD.py
+++ Codeforces/Level-A/Maximum GCD.py
@@ -1,3 +1,6 @@
+# Problem: Maximum GCD (Codeforces 1370A)
+# Tags: Greedy, Implementation, Math, Number theory
+# Link: https://codeforces.com/problemset/problem/1370/A
 t = int(input())
 for _ in range(t):
     n=int(input())
