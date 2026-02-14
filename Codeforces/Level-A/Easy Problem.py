dd241b756cf1258f6bccbb23974c79b1cfbfee03
mernakabass <mernaabass@outlook.com>
2026-02-14 23:18:40 +0200

    Solved: Easy Problem [Brute Force, Math]

---
+++ Codeforces/Level-A/Easy Problem.py
@@ -0,0 +1,8 @@
+# Problem: Easy Problem (Codeforces 2044A)
+# Tags: Brute Force, Math
+# Link: https://codeforces.com/problemset/problem/2044/A
+# Logic: The number of pairs is always n - 1
+t=int(input())
+for _ in range(t):
+    n=int(input())
+    print(n-1)
