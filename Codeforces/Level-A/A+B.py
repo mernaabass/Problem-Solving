00ba872ed455f0586817ca713e65566d7331f5c1
mernakabass <mernaabass@outlook.com>
2026-02-16 15:03:24 +0200

    Solved: A+B [Implementation]

---
+++ Codeforces/Level-A/A+B.py
@@ -0,0 +1,7 @@
+# Problem: A+B (Codeforces 1772A)
+# Tags: Implementation
+# Link: https://codeforces.com/problemset/problem/1772/A
+t=int(input())
+for _ in range(t):
+    s=input()
+    print(int(s[0])+int(s[2]))
