a4e15422321206686eac873b36bf4d66f9fd51ab
mernakabass <mernaabass@outlook.com>
2026-02-24 16:36:01 +0200

    Solved: Bachgold Problem [Greedy, Implementation, Math, Number theory]

---
+++ Codeforces/Level-A/Bachgold Problem.py
@@ -0,0 +1,12 @@
+# Problem: Bachgold Problem (Codeforces 1535A)
+# Tags: Greedy, Implementation, Math, Number Theory
+# Link: https://codeforces.com/problemset/problem/749/A
+# Logic: Use as many 2s as possible. If odd, use one 3 and the rest 2s.
+n=int(input())
+k=n//2
+print(k)
+if n%2==0:
+    lst=k*[2]
+else:
+    lst=(k-1)*[2]+[3]    
+print(*lst)    
