bb2578aaa8a796be8cec4b62b6d71ed6bd2dff54
mernakabass <mernaabass@outlook.com>
2026-03-04 15:40:56 +0200

    Solved: Mahmoud and Ehab and the even-odd game [Games, Math]

---
+++ Codeforces/Level-A/Mahmoud and Ehab and the even-odd game.py
@@ -0,0 +1,8 @@
+# Problem: Mahmoud and Ehab and the even-odd game (Codeforces 959A)
+# Tags: Games, Math
+# Link: https://codeforces.com/problemset/problem/959/A
+n=int(input())
+if n%2 ==0:
+    print("Mahmoud")
+else:
+    print("Ehab")    
