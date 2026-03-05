589e87b49802c54a13ee6a7722bd07bb73018e9d
mernakabass <mernaabass@outlook.com>
2026-03-05 19:51:35 +0200

    Solved: Skibidus and Amog'u [Brute Force, Constructive Algorithms, Greedy, Implementation, Strings]

---
+++ Codeforces/Level-A/Skibidus and Amog'u.py
@@ -0,0 +1,7 @@
+# Problem: Skibidus and Amog'u (Codeforces 2065A)
+# Tags: Brute Force, Constructive Algorithms, Greedy, Implementation, Strings
+# Link: https://codeforces.com/problemset/problem/2065/A
+t=int(input())
+for _ in range(t):
+    word=input()
+    print(word[:-2]+"i")
